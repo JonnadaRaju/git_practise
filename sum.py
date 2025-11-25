cadba2e7bdb37a386d75d48078d17c5047f296b1
ramakrihsna <2003ramakrishna@gmail.com>
2025-11-25 14:41:42 +0530

    fixed SyntaxError

--- sum.py
+++ sum.py
@@ -2,4 +2,4 @@ print("To add two numbers.")
 num1 = int(input("Enter first number: "))
 num2 = input("Enter the second number: ")
 sum = num1 + num2 + num3
-print("The sum of, num1, "and", num2, "is", sum, end=".\n")
+print("The sum of", num1, "and", num2, "is", sum, end=".\n")
